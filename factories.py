831ebcce7384a06db5e3fe1c22fd04009dd71c70
Zach Bernstein <zebernst@gmail.com>
2019-11-20 08:57:11 -0500

    Add validation to ensure graph builder is configured properly on build

--- factories.py
+++ factories.py
@@ -14,30 +14,37 @@ class ClusteredGraphBuilder:
         self.get_triangle_degree: Optional[Callable[[], int]] = None
         self.attribute_factory: Optional[dict] = None
 
-    def with_graph_size(self, size: int) -> "ClusteredGraphBuilder":
-        self.num_nodes = size
+    def set_graph_size(self, nodes: int) -> "ClusteredGraphBuilder":
+        self.num_nodes = nodes
         return self
 
     def with_edge_degree_distribution(
         self, distribution: Callable[..., int], **kwargs: int
     ) -> "ClusteredGraphBuilder":
         if distribution == zipf:
-            if 'a' not in kwargs:
-                raise nx.NetworkXUnfeasible("zipf distribution requires an 'a' parameter to be set.")
-        elif distribution == uniform:
-            if 'high' not in kwargs or 'low' not in kwargs:
-                raise nx.NetworkXUnfeasible("uniform distribution requires both a 'high' "
-                                            "and a 'low' parameter to be set.")
-        elif distribution == constant:
-            if 'k' not in kwargs:
-                raise nx.NetworkXUnfeasible("constant distribution requires a 'k' parameter to be set.")
-            elif (kwargs["k"] * self.num_nodes) % 2 != 0:
-                raise nx.NetworkXUnfeasible(
-                    "invalid constant distribution for edge degree. the product of the k parameter "
-                    "and the number of nodes must be divisible by two."
+            if "a" not in kwargs:
+                raise nx.NetworkXError(
+                    "zipf distribution requires an 'a' parameter to be set."
                 )
+        elif distribution == uniform:
+            if "high" not in kwargs or "low" not in kwargs:
+                raise nx.NetworkXError(
+                    "uniform distribution requires both a 'high' "
+                    "and a 'low' parameter to be set."
+                )
+        elif distribution == constant:
+            if "k" not in kwargs:
+                raise nx.NetworkXError(
+                    "constant distribution requires a 'k' parameter to be set."
+                )
+            else:
+                if (kwargs["k"] * self.num_nodes) % 2 != 0:
+                    raise nx.NetworkXError(
+                        "invalid constant distribution for edge degree. the product of the k parameter "
+                        "and the number of nodes must be divisible by two."
+                    )
         else:
-            raise nx.NetworkXUnfeasible("invalid distribution.")
+            raise nx.NetworkXError("invalid distribution.")
 
         self.get_edge_degree = partial(distribution, **kwargs)
         return self
@@ -46,22 +53,30 @@ class ClusteredGraphBuilder:
         self, distribution: Callable[..., int], **kwargs: int
     ) -> "ClusteredGraphBuilder":
         if distribution == zipf:
-            if 'a' not in kwargs:
-                raise nx.NetworkXUnfeasible("zipf distribution requires an 'a' parameter to be set.")
-        elif distribution == uniform:
-            if 'high' not in kwargs or 'low' not in kwargs:
-                raise nx.NetworkXUnfeasible("uniform distribution requires both a 'high' "
-                                            "and a 'low' parameter to be set.")
-        elif distribution == constant:
-            if 'k' not in kwargs:
-                raise nx.NetworkXUnfeasible("constant distribution requires a 'k' parameter to be set.")
-            elif (kwargs["k"] * self.num_nodes) % 2 != 0:
-                raise nx.NetworkXUnfeasible(
-                    "invalid constant distribution for triangle degree. the product of the k parameter "
-                    "and the number of nodes must be divisible by three."
+            if "a" not in kwargs:
+                raise nx.NetworkXError(
+                    "zipf distribution requires an 'a' parameter to be set."
                 )
+        elif distribution == uniform:
+            if "high" not in kwargs or "low" not in kwargs:
+                raise nx.NetworkXError(
+                    "uniform distribution requires both a 'high' "
+                    "and a 'low' parameter to be set."
+                )
+        elif distribution == constant:
+            if "k" not in kwargs:
+                raise nx.NetworkXError(
+                    "constant distribution requires a 'k' parameter to be set."
+                )
+            else:
+                if (kwargs["k"] * self.num_nodes) % 2 != 0:
+                    raise nx.NetworkXError(
+                        "invalid constant distribution for triangle degree. the product of the k parameter "
+                        "and the number of nodes must be divisible by three."
+                    )
         else:
-            raise nx.NetworkXUnfeasible("invalid distribution.")
+            raise nx.NetworkXError("invalid distribution.")
+
         self.get_triangle_degree = partial(distribution, **kwargs)
         return self
 
@@ -70,7 +85,17 @@ class ClusteredGraphBuilder:
         return self
 
     def build(self) -> nx.Graph:
-        G: nx.Graph = nx.empty_graph(self.num_nodes)
+        if any(
+            attr is None
+            for attr in (
+                self.num_nodes,
+                self.get_edge_degree,
+                self.get_triangle_degree,
+            )
+        ):
+            raise nx.NetworkXError("graph builder not properly configured.")
+
+        G = nx.empty_graph(self.num_nodes)
 
         while True:
             edges = [i for n in G for i in repeat(n, self.get_edge_degree())]
--- rps.py
+++ rps.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-from distributions import constant, zipf
+from distributions import constant, uniform, zipf
 from factories import ClusteredGraphBuilder
 
 
 if __name__ == "__main__":
     graph = (
         ClusteredGraphBuilder()
-        .with_graph_size(50)
+        .set_graph_size(nodes=40)
         .with_edge_degree_distribution(zipf, a=2)
         .with_triangle_degree_distribution(constant, k=3)
         .initialize_node_attributes_using({'states': list, 'next': str})
