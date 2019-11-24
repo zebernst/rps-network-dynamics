ba1ad39227ddab53a74d640e3e11aedc10cf0066
Erik Brown <ebrown50@uvm.edu>
2019-11-24 15:04:17 -0500

    initial code for determining cycles of convergence

--- rps.py
+++ rps.py
@@ -19,13 +19,27 @@ def optimize_choice(states: list):
     else:
         raise Exception("invalid state!")
 
+def conv_cycle(states: list):
+    length=2
+    converged=False
+    while not converged and length<20:
+        converged=True
+        for i in range(1,length+1):
+            matchstate=states[-i]
+            if states[-(i+length)] != matchstate:
+                converged=False
+        length +=1
+    if converged:
+        return length-1
+    else:
+        raise Exception("Network did not Converge! Run for more itterations")
 
 if __name__ == "__main__":
     graph = (
         ClusteredGraphBuilder()
-        .set_graph_size(nodes=40)
-        .with_edge_degree_distribution(zipf, a=2)
-        .with_triangle_degree_distribution(constant, k=3)
+        .set_graph_size(nodes=100)
+        .with_edge_degree_distribution(uniform, low=1, high=6)
+        .with_triangle_degree_distribution(uniform, low=0, high=3)
         .initialize_node_attributes_using({'states': list, 'next': str})
         .build()
     )
@@ -46,4 +60,8 @@ if __name__ == "__main__":
     nx.draw_kamada_kawai(graph)
     plt.show()
 
+    conv_cycles=[]
+    for node, data in graph.nodes(data=True):
+        conv_cycles.append(conv_cycle(data['states']))
+    print(conv_cycles)
 
