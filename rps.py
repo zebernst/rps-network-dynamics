6f679ea18c95d35e513c03b265bc5acbcfe54702
Zach Bernstein <zebernst@gmail.com>
2019-11-24 13:43:30 -0500

    implement initial rock-paper-scissors network interactions

--- rps.py
+++ rps.py
@@ -1,8 +1,23 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+import random
 
 from distributions import constant, uniform, zipf
 from factories import ClusteredGraphBuilder
+from collections import Counter
+
+
+def optimize_choice(states: list):
+    most_common = Counter(states).most_common(1)[0][0]
+
+    if most_common.lower() == "rock":
+        return "paper"
+    elif most_common.lower() == "paper":
+        return "scissors"
+    elif most_common.lower() == "scissors":
+        return "rock"
+    else:
+        raise Exception("invalid state!")
 
 
 if __name__ == "__main__":
@@ -15,5 +30,20 @@ if __name__ == "__main__":
         .build()
     )
 
+    for node, data in graph.nodes(data=True):
+        data['states'].append(random.choice(("rock", "paper", "scissors")))
+
+    for _ in range(50):
+        for node, data in graph.nodes(data=True):
+            neigh = graph[node]
+            states = [graph.nodes[n]['states'][-1] for n in neigh]
+            data["next"] = optimize_choice(states)
+
+        for node, data in graph.nodes(data=True):
+            data['states'].append(data['next'])
+            data['next'] = ""
+
     nx.draw_kamada_kawai(graph)
     plt.show()
+
+
